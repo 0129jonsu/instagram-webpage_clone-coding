a4ee63ee4d49eedfe3f51701089696a50ff83881
JunSuKim <12191747@inha.edu>
2022-11-17 17:57:36 +0900

    2022-11-17 sign-in check

--- sign-up.js
+++ sign-up.js
@@ -48,6 +48,7 @@ app.post( '/sign_up', (req ,res) => {
 });
 
 app.post( '/sign_in', (req ,res) => {
+    req.body;
     const conn = {  // mysql 접속 설정
         host: 'user-data.cmagpshmnsos.ap-northeast-2.rds.amazonaws.com',
         port: '3306',
@@ -57,19 +58,44 @@ app.post( '/sign_in', (req ,res) => {
     };
 
     var connection = mysql.createConnection(conn);
-    
     connection.connect();
-    
+
+    let sql_check_id = "select * from user_data.sign_up_table where user_id = ?"
+    let check_id = {user_id : req.body.sign_in_id}
+    var sign_in_check_id = connection.query(sql_check_id, check_id , function (err, results, fields) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    let sql_check_pw = "select * from user_data.sign_up_table where user_pw = ?"
+    let check_pw = {user_pw : req.body.sign_in_pw}
+    var sign_in_check_pw = connection.query(sql_check_pw, check_pw , function (err, results, fields) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
     let sql = "select * from user_data.sign_up_table";
     var sign_up_data = connection.query(sql, function (err, results, fields) {
         if (err) {
             console.log(err);
         }
-        console.log(results);
     });
-
-    console.log(sign_up_data.user_numorem)
+    //console.log(req.body.sign_in_id, req.body.sign_in_pw)
+    if(sign_in_check_id._results == '' || sign_in_check_pw._results == '') {
+        res.write("<script>alert('id or pw wrong')</script>");
+        console.log('틀림')
+    }
+    else {
+        res.write("<script>alert('login success!')</script>");
+        alert('login!')
+    }
+    //[ [] ]
 });
+
+
+
 
 app.get( '/db'/*라우팅*/, (req ,res) => {
     const conn = {  // mysql 접속 설정
@@ -83,14 +109,13 @@ app.get( '/db'/*라우팅*/, (req ,res) => {
     var connection = mysql.createConnection(conn);
     
     connection.connect();
-    
+
     let sql = "select * from user_data.sign_up_table";
     var sign_up_data = connection.query(sql, function (err, results, fields) {
         if (err) {
             console.log(err);
         }
-        console.log(results);
     });
 
-    console.log(sign_up_data.user_numorem)
+    console.log(sign_up_data)
 });
